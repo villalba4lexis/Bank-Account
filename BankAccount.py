2eb9ff6a1ed1234303cdd2b114dc0bd65c253d94
Alexis Arenas-Villalba <alexisa.villalba05@icloud.com>
2025-07-03 19:59:16 -0400

    Updated SavingAccount.py class by implementing 'interest' method - adding intrest to customers balance based on their current balance.

--- BankAccount.py
+++ BankAccount.py
@@ -1,52 +1,54 @@
 class BankAccount:
     ## Global Attribute
-    customerBank = "Wells Fargo"
+    _customerBank = "Wells Fargo"
 
     ## Initializing Attributes for BankAccount Class, Created Private Members
     def __init__(self, name, balance=0.0):
-        self.__customerName = name
-        self.__customerBalance = balance
-        self.__customerMinBalance = 50.0
+        self._customerName = name
+        self._customerBalance = balance
+        self._customerMinBalance = 50.0
 
-    ## Getter Methods to Access Private Memebers
+    ## Getter Methods to Access Protected Members
+    def getCustomerBank(self):
+        return self._customerBank
     def getCustomerName(self):
-        return self.__customerName
+        return self._customerName
 
     def getCustomerBalance(self):
-        return self.__customerBalance
+        return self._customerBalance
 
     def getCustomerMinBalance(self):
-        return self.__customerMinBalance
+        return self._customerMinBalance
 
     ##Assignments Function Requirements
     def deposit(self, amount):
         if amount <= 0:
             print("Deposit Failed: PLEASE DEPOSIT VALID VALUE.")
         else:
-            self.__customerBalance += amount
-            print(f"Deposit Successful:\n    NEW BALANCE {self.__customerBalance}")
+            self._customerBalance += amount
+            print(f"Deposit Successful:\n    NEW BALANCE {self._customerBalance}")
 
     def withdraw(self, amount):
         if amount <= 0:
             print("Withdraw Failed: PLEASE ENTER VALID VALUE.")
-        elif self.__customerBalance - amount < self.__customerMinBalance:
-            print(f"Withdraw Failed:\n    CANNOT WITHDRAW MINIMUM BALANCE: {self.__customerMinBalance}.")
+        elif self._customerBalance - amount < self._customerMinBalance:
+            print(f"Withdraw Failed:\n    CANNOT WITHDRAW MINIMUM BALANCE: {self._customerMinBalance}.")
         else:
-            self.__customerBalance -= amount
-            print(f"Withdraw Successful: New Balance is {self.__customerBalance}")
+            self._customerBalance -= amount
+            print(f"Withdraw Successful: New Balance is {self._customerBalance}")
 
     def print_customer_information(self):
         print("BankAccount Information")
         print("------------------------------------------")
         print(f"Bank Title: {BankAccount.customerBank}")
-        print(f"Customer Name: {self.__customerName}")
-        print(f"Account Balance: {self.__customerBalance}")
-        print(f"Minimum Required Balance: {self.__customerMinBalance}")
+        print(f"Customer Name: {self._customerName}")
+        print(f"Account Balance: {self._customerBalance}")
+        print(f"Minimum Required Balance: {self._customerMinBalance}")
 
     def __str__(self):
         return (f"BankAccount Information\n"
                 f"------------------------------------------\n"
                 f"Bank Title: {BankAccount.customerBank}\n"
-                f"Customer Name: {self.__customerName}\n"
-                f"Account Balance: {self.__customerBalance}\n"
-                f"Minimum Required Balance: {self.__customerMinBalance}")
+                f"Customer Name: {self._customerName}\n"
+                f"Account Balance: {self._customerBalance}\n"
+                f"Minimum Required Balance: {self._customerMinBalance}")
--- SavingAccount.py
+++ SavingAccount.py
@@ -1,4 +1,28 @@
 from BankAccount import BankAccount
+import random
 
 class SavingAccount(BankAccount):
-    pass
+    ## Initializing Attributes for SavingAccount Sub-Class w/ Private members
+    def __init__(self, name, balance=0.0,):
+        BankAccount.__init__(self, name, balance)
+        self.__routingNumber = random.randint(1, 20) ## Random Numbers Generated for Routing Number
+        self.__accountNumber = random.randint(1, 10) ## Random Numbers Generated for Account Number
+
+    ## Access to Private Members: 'routingNumbers' and 'accountNumber'
+    def getRoutingNumber(self):
+        return self.__routingNumber
+    def getAccountNumber(self):
+        return self.__accountNumber
+
+    ## Assignment Requirements: Adding Intrest
+    def interest(self):
+        balance = self.getCustomerBalance()
+        if balance == 0:
+            rate = 0.0
+        elif balance < 500:
+            rate = 0.02
+        else:
+            rate = 0.05
+
+        interest = balance * rate
+        print(f"Current Balance with {self.getCustomerName()}: {interest:.2f}")
